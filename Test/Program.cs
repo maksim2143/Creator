c6ebc143230317a8dcaf20c9ac8c84724f15fab9
new_user <new_user@gmail.com>
2021-03-03 19:51:08 +0200

    12345

--- Test/Program.cs
+++ Test/Program.cs
@@ -9,27 +9,39 @@ using System.Reflection;
 
 namespace Test
 {
+    class f { }
     class Program
     {
         static void Main(string[] args)
         {
             var dict = new Dictionary<string, Type>()
             {
+                {"Id",typeof(f) },
+              
+            };
+            var resultK = Generator.CreateType("new66Type", dict); 
+            var jytjyj = new Dictionary<string, Type>()
+            {
                 {"Id",typeof(int) },
-                {"FirstName",typeof(string) },
+                {"FirstName", resultK},
                 {"LastName",typeof(string) }
             };
-            var result = Generator.CreateType("newType", dict);
+            Type result = Generator.CreateType("newType", jytjyj);
             var obj = Activator.CreateInstance(result);
             obj.GetType()
                 .GetRuntimeProperties()
                 .ToList()
                 .ForEach(q =>
                 {
-                    Console.WriteLine("Name="+q.Name+" Type="+q.PropertyType.ToString());
+                    Console.WriteLine("Name=" + q.Name + " Type=" + q.PropertyType.ToString());
                 });
             Console.WriteLine("OK");
             Console.ReadKey();
         }
+    }
+    class I { }
+    class J
+    {
+        public I i { get; }
     }
 }
